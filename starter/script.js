73552314d06933910ad4cb5927910ad019e04269
ElisaMavilia <elisamavilia1@gmail.com>
2024-11-20 15:46:54 +0100

    implementing the basic logic

--- starter/script.js
+++ starter/script.js
@@ -12,11 +12,30 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
  */
 
+const Secretnumber = Math.trunc(Math.random()*20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = Secretnumber;
+
+
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
     if (!guess) { //if it is false = no value in the input
         document.querySelector('.message').textContent = 'No number!'
-    }
-}); //it selects the btn check, adds the click event and passes the function value we want
+    } else if ( guess === Secretnumber ){
+        document.querySelector('.message').textContent = 'Correct Number!'
+        score = score + 5;
+        document.querySelector('.score').textContent = score;
+    } else if (guess < Secretnumber){
+        if (score > 1){
+            document.querySelector('.message').textContent = 'The number is to low!'
+            score--;
+        }else {
+            document.querySelector('.message').textContent = 'You lose the game!'
+            document.querySelector('.score').textContent = score = 0;
+        }
+        
+        document.querySelector('.score').textContent = score;
+    } 
+}); 
